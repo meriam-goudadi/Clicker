7dfc75a95cd0395edf4152a8c1914871424c6a9f
Alex.Bachir <alex.bachir@laplateforme.io>
2025-04-09 11:02:18 +0200

    mise a jour fonction

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ let parsedDiamond = parseFloat(diamond.innerHTML)
 // parse convertis les chaine de charactere en nombre pour faire des calcules
 let clickerCost = document.querySelector(".clickerCost")
 let parsedClickerCost = parseFloat(clickerCost.innerHTML)
-
 let clickerLevel = document.querySelector(".clicker-level")
 let clickerIncrease = document.querySelector(".clicker-increase")
 let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
 
-// dpc = diamand par clic
+//  variable pour pickaxe
+
+ let pickaxeCost = document.querySelector(".pickaxeCost")
+ let parsedPickaxeCost = parseFloat(pickaxeCost.innerHTML)
+ let pickaxeLevel = document.querySelector(".pickaxe-level")
+ let pickaxeIncrease = document.querySelector(".pickaxe-increase")
+ let parsedPickaxeIncrease = parseFloat(pickaxeIncrease.innerHTML)
+
+ 
 
 let dpc = 1
 
@@ -36,6 +43,28 @@ function buyClick() {
 
     parsedClickerCost *= 1.18
     clickerCost.innerHTML = Math.round(parsedClickerCost)
+    }
+
+
+    
+}
+
+
+//  fonction permet d'acheter des pioche 
+
+function buyPickaxe() {
+    if (parsedDiamond >= parsedPickaxeCost) {
+    diamond.innerHTML = Math.round(parsedDiamond -= parsedPickaxeCost)
+
+
+    pickaxeLevel.innerHTML ++
+
+    parsedPickaxeIncrease = parseFloat((parsedPickaxeIncrease * 1.03).toFixed(2))
+    pickaxeIncrease.innerHTML = parsedPickaxeIncrease 
+    dpc += parsedPickaxeIncrease
+
+    parsedPickaxeCost *= 1.18
+    pickaxeCost.innerHTML = Math.round(parsedPickaxeCost)
     }
 
 
